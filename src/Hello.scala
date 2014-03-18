56aac801e555a512988f2c6456a8bc4834e2f31f
sat <sat@sandiego.edu>
2014-03-18 15:22:23 -0700

    added sat's checkin message

--- src/Hello.scala
+++ src/Hello.scala
@@ -3,6 +3,7 @@ object Hello {
   def main(args: Array[String]): Unit = {
     println("COMP 360 Github Checkin")
     // put your "hello" message after here
+    println("Dr. Sat wuz here!")
   }
 
 }
